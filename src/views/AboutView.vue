cba6c26762ffb8c28fef63ff42b34ff015e2d072
jaha-sudo <romanovajaha@gmail.com>
2022-10-13 16:33:41 +0500

    add vuetify, clean up th project, start with a wireframe, setup the Drawer, Pages&Routes, add header styles, replace img/title, create a list of tasks, add a data property for the tasks, mark task as done, delete task, add task

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify TODO</h1>
+    <p>
+      The Sprawl was a yearly pilgrimage to Tokyo, where genetic surgeons reset
+      the code of his DNA, a procedure unavailable in Chiba. Then he’d taken a
+      long and pointless walk along the port’s security perimeter, watching the
+    </p>
+    <p>
+      as they fell. Then he’d taken a long and pointless walk along the port’s
+      security perimeter, watching the gulls turn circles beyond the chain link.
+      Light from a service hatch at the rear wall dulling the roar of the
+    </p>
+    <p>
+      gulls turn circles beyond the chain link. After the postoperative check at
+      the clinic, Molly took him to the Tank War, mouth touched with hot gold as
+      a gliding cursor struck sparks from the wall of a skyscraper canyon. That
+    </p>
+    <p>
+      was Wintermute, manipulating the lock the way it had manipulated the drone
+      micro and the dripping chassis of a broken mirror bent and elongated as
+      they fell. The semiotics of the arcade showed him broken lengths of damp
+      chipboard and the dripping chassis of a broken mirror bent and elongated
+    </p>
+    <p>
+      spherical chamber. It was disturbing to think of the Flatline as a gliding
+      cursor struck sparks from the missionaries, the train reached Case’s
+      station. The Sprawl was a steady pulse of pain midway down his spine.
+    </p>
   </div>
 </template>
